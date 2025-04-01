407a9f0e879fe31ed0cabce62483dca13c387633
Malo-35 <malo.barbaud@isen.yncrea.fr>
2025-04-01 10:30:22 +0200

    Initialisation de la branche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ApiserviceService } from './apiservice.service';
+import { AProposComponent } from './a-propos/a-propos.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
+import { PersonnagesComponent } from './personnages/personnages.component';
+import { PlanetesComponent } from './planetes/planetes.component';
+import { VaisseauxComponent } from './vaisseaux/vaisseaux.component';
+import { ApiserviceService } from './apiservice.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
